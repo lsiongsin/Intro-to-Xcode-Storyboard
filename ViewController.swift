08b916c085b788a4990aba8c52826be9c12d4c44
lsiongsin <110996784+lsiongsin@users.noreply.github.com>
2022-08-10 15:10:25 -0400

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Intro-to-Xcode&Storyboard
+//
+//  Created by Scholar on 8/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("This is a print statement - testing testing!")
+        print("hello hello")
+    }
+
+
+}
+
